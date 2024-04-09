79d2c121786517c28289350d0b157af5513572d1
Thais Menezes <thais.mas@hotmail.com>
2024-04-08 22:18:37 -0300

    feat: tornando a funcao autoexecutavel e adicionando fs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-async function scrapeData() {
+(async() => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://pt.wikipedia.org/wiki/Lista_de_municípios_do_Brasil_por_população_(2022)');
@@ -16,14 +17,14 @@ async function scrapeData() {
             population: columns[4].innerText.trim()
         }
       }
-    }).filter(Boolean);
+    }).slice(1);
   });
 
+  fs.writeFile('citiesList.json', JSON.stringify(cities, null, 2), err => {
+      if(err) throw new Error('Ops, algo deu errado');
+      console.log('Certinho!')
+  })
+
   await browser.close();
+})();
 
-  return cities;
-}
-
-scrapeData().then(cities => {
-  console.log("Dados extraídos com sucesso:", cities);
-}).catch(console.error);
